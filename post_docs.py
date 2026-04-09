d0d3f07c73b36d7b1101e0e54cd743e06eb42274
Emily Przykucki <emily.przykucki@gmail.com>
2026-04-09 17:48:13 -0400

    add post_docs.py

---
+++ post_docs.py
@@ -0,0 +1,29 @@
+# a file to generate random ages for post docs
+import random
+import pandas as pd
+
+POST_DOCS = [
+    "Joe",
+    "Moriah",
+    "Praachi",
+    "Matt",
+    "Seungwon",
+    "Abby",
+    "Claire"
+]
+FOOD = [
+    "plums",
+    "pizza",
+    "pasta",
+    "peanut butter",
+    "popsicles"
+]
+
+
+def main():
+    """Main execution function"""
+    pass
+
+
+if __name__ == '__main__':
+    main()
